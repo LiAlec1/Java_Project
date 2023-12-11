67d890a35a2639dfe100149bf283989f29f37961
LiAlec1 <136616448+LiAlec1@users.noreply.github.com>
2023-12-11 11:14:13 -0500

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    final int ORIGINAL_TILE_SIZE = 16;
+    final int SCALE = 3;
+    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+
+    // Define screen as 4:3 ratio
+    final int MAX_SCREEN_COL = 16;
+    final int MAX_SCREEN_ROW = 12;
+    final int WINDOW_WIDTH = TILE_SIZE * MAX_SCREEN_COL;
+    final int WINDOW_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
+
+    Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    int FPS = 60;
+
+    Player player;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // Improve render performance
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+
+        player = new Player(this, keyHandler);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000 / FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+
+            System.out.println("game loop");
+
+            update();
+
+            repaint();
+
+            try {
+
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000; // Convert nanoseconds to miliseconds
+
+                remainingTime = Math.max(0, remainingTime);
+
+                nextDrawTime += drawInterval;
+
+                Thread.sleep((long) remainingTime);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+
+        player.update();
+
+    }
+
+    public void paintComponent(Graphics graphic) {
+
+        super.paintComponent(graphic);
+
+        Graphics2D altGraphic = (Graphics2D) graphic;
+
+        player.draw(altGraphic);
+
+        altGraphic.dispose();
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
-// Comment
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class Main {
+    public static void main(String args[]) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Game Name");
+
+        GamePanel panel = new GamePanel();
+        window.add(panel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null); // Set window location to center
+        window.setVisible(true);
+
+        panel.startGameThread();
     }
 }
